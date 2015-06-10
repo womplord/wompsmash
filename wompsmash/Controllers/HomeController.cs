07f488ea15c5e01522db123bda6c8f97c712e233
womplord <jeremy186@hotmail.com>
2015-06-10 23:29:31 +1000

    started working on css and added a blogpost model

---
+++ wompsmash/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using wompsmash.Models;
+
+namespace wompsmash.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var blogPost = new List<BlogPost>();
+
+            if(Session["BlogPost"] != null)
+            {
+                blogPost.Add((BlogPost)Session["BlogPost"]);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBlog(BlogPost newBlogPost)
+        {
+            var blogPost = new List<BlogPost>();
+            if(Session["BlogPost"] != null)
+            {
+                blogPost.Add((BlogPost)Session["BlogPost"]);
+            }
+
+            blogPost.Add(newBlogPost);
+            Session["BlogPost"] = blogPost;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ wompsmash/Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wompsmash.Models
+{
+    public class BlogPost
+    {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
